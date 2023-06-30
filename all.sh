a44635a7709b3a45119a127bb145192c21c5a285
shankar acharya <34772838+5hank4r@users.noreply.github.com>
2023-06-30 20:11:58 +0545

    Update all.sh

--- all.sh
+++ all.sh
@@ -4,7 +4,7 @@
 read -p "Enter the subdomain file name (including .txt extension): " subdomain_file
 
 # Prompt for variable name
-read -p "Enter the variable name: " Target_Name
+read -p "Enter the variable name: " variable_name
 
 output_directory="output"
 katana_output="$output_directory/$variable_name.katana.txt"
@@ -29,7 +29,7 @@ echo -e "\e[1m\e[32mGau completed. Output saved to $gau_output\e[0m"
 
 #Run GoSpider
 echo -e "\e[1m\e[32mRunning GoSpider...\e[0m"
-gospider -S $subdomain_file -c 10 -d 1 -a | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | tee "$gospider_output" 
+gospider -S $subdomain_file -c 10 -d 1 -a | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" > "$gospider_output" 
 echo -e "\e[1m\e[32mGoSpider completed. Output saved to $gospider_output\e[0m"
 
 # Run waybackurl command and save output to variable_name.waybackurls.txt
@@ -39,5 +39,4 @@ echo -e "\e[1m\e[32mwaybackurl completed. Output saved to $waybackurls_output\e[
 
 # Combine the results into variable_name.final.txt
 cat "$katana_output" "$gau_output" "$gospider_output" "$waybackurls_output" > "$final_output"
-
 echo -e "\e[1m\e[32mResults saved to $final_output\e[0m"
